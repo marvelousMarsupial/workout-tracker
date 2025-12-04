4d63818c271e5f313950d84b9991a3b633fae589
marvelousMarsupial <me@augusttippett.com>
2025-12-03 17:24:08 -0800

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <UContainer class="flex justify-center items-center min-h-screen">
     <UCard>
-      <p>Holla Mundo!</p>
+      <p>Holla Mundo! </p>
     </UCard>
   </UContainer>
 </template>
